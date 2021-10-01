3ac95f501f7e36c127236047c68f3e958c269052
Junior Lopes <juniorlopes@live.jp>
2021-10-01 20:12:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Atividade_5_K_means
+ Atividade 5  Inteligência Computacional
---
+++ atividade_5_icom.py
@@ -0,0 +1,37 @@
+"""
+Created on Thu Sep 27 17:54:19 2021
+
+# Equipe:
+# *   Juliane
+# *   Rubens Lopes
+
+"""
+
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the Iris dataset with pandas
+parkinson = pd.read_csv('D:/OneDrive/Faculdade/S7/parkinson/parkinson_formated.csv')
+
+data = parkinson
+
+kmeans = KMeans(n_clusters = 2, init = 'random')
+
+kmeans.fit(data)
+
+kmeans.cluster_centers_
+
+distance = kmeans.fit_transform(data)
+
+labels = kmeans.labels_
+
+data = data.iloc[:,:-1].values
+
+plt.scatter(data[:, 0], data[:,1], s = 100, c = kmeans.labels_)
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],s = 100, c = 'red',label = 'Centroids')
+plt.title('Parkinson Clusters and Centroids')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend('Parkinson')
+plt.show()
